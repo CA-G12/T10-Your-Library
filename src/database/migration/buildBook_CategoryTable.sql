50569d7e9910fd92be778813f027978230eed7b1
Saeed99Madi <saed.zooro@gmail.com>
2022-08-30 15:36:33 +0300

    build categoy_book table related to #9

---
+++ src/database/migration/buildBook_CategoryTable.sql
@@ -0,0 +1,12 @@
+BEGIN;
+
+DROP TABLE IF EXISTS book_category CASCADE;
+
+CREATE TABLE book_category(
+    id SERIAL PRIMARY KEY,
+    category_id INTEGER NOT NULL REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    book_id INTEGER NOT NULL REFERENCES books(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT book_category_pkey PRIMARY KEY (category_id , book_id)
+);
+
+COMMIT;
