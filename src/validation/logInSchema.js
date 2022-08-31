8e444e96a2a9e36c10d33d15ef23d0c561f335a3
Saeed99Madi <saed.zooro@gmail.com>
2022-08-31 16:32:50 +0300

    handle login route and controller related to #26

--- src/validation/logInSchema.js
+++ src/validation/logInSchema.js
@@ -1,11 +1,8 @@
 const Joi = require('joi');
 
 const loginSchema = Joi.object({
-  username: Joi.string().alphanum().min(7).max(20)
-    .required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(8).max(20).pattern(/^[a-zA-Z0-9]$/)
-    .required(),
+  password: Joi.string().min(8).max(20).required(),
 });
 
 module.exports = loginSchema;
